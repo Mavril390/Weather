4fe6f35c441df5a51eb209de87b2c787a128fc7e
Mauricio Marinkovich <maumarinkovich@gmail.com>
2021-05-24 18:15:05 -0300

    Se modifica de HTTP a HTTPS

--- src/Components/WeatherToday/apiWeatherToday.js
+++ src/Components/WeatherToday/apiWeatherToday.js
@@ -2,7 +2,7 @@ const API_KEY = 'e62712f5a8506ba9f4d2050b7b6f7f49'
 
 const fetchData = position => {
   const { latitude, longitude } = position.coords;
-  fetch(`http://api.openweathermap.org/data/2.5/weather?units=metric&lat=${latitude}&lon=${longitude}&appid=${API_KEY}`)
+  fetch(`https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${latitude}&lon=${longitude}&appid=${API_KEY}`)
     .then(response => response.json())
     .then(data => setWeatherDataToday(data));
 }
